655bff28e63b377ad97c6d1af4b37bf7cf0623b8
pritongdinosaur <ceejaycutie11@gmail.com>
2026-05-06 16:02:31 +0800

    5

---
+++ constructor_and_destructor/problem_5.php
@@ -0,0 +1,39 @@
+/*
+Problem 2: The Discounted Shopping CartScenario: An e-commerce site wants to apply a "Welcome Discount" automatically as
+soon as a cart is created for a new user.
+Task: Create a ShoppingCart class.
+Properties: $total and $discount.
+The Constructor:Accepts a $discountPercentage (e.g., 10 for 10%).
+Sets the initial $total to 0.
+Prints: "New cart initialized with a [X]% discount code.
+"Method addItem($price):Adds the price to the $total.
+The Destructor:Calculates the final price after the discount: $Final = Total \times (1 - \frac{Discount}{100})$.
+Prints: "Final Checkout Amount: $[Final Price]."
+*/
+
+<?php
+
+class ShoppingCart
+{
+    public $total = 0;
+    public $discount;
+    public function __construct($discountPercentage)
+    {
+        $this->discount = $discountPercentage;
+        echo "New cart initialized with a $discountPercentage% discount code.";
+    }
+    public function addItem($price)
+    {
+        $this->total += $price;
+    }
+
+    public function __destruct()
+    {
+        $final = $this->total * (1 - ($this->discount / 100));
+        echo "Final Checkout Amount: $" . number_format($final, 2) . "\n";
+    }
+}
+
+$cart = new ShoppingCart(10);
+$cart->addItem(100);
+$cart->addItem(50);
